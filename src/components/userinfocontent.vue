5dad46804b220d73a4a025c07f17820add47779d
onat <onatbudak@gmail.com>
2024-09-15 00:19:09 +0300

    Usta yetiscek

--- src/components/userinfocontent.vue
+++ src/components/userinfocontent.vue
@@ -1,113 +1,112 @@
 <template>
-    <div class="container">
-      <span class="userInfoBox">
-        <div v-for="(user,index) in listResult">
-            <h1>Your Info</h1>
-            <h2 class="email">{{user.email }} is your email</h2>
-            <label for="fn1">Your firstname</label>
-            <input class="fn" readonly :value="user.firstname">
-            <br>
-            <label for="fn1">Your lastname</label>
-            <input class="fn" :value="user.lastname" readonly>
-            <br>
-        </div>
-        <button @click="GetInfo">Get Your Info</button>
-      </span>
-      <span class="userInfoBox">
-        <div v-for="(user,index) in listResult">
-            <h1>Change Here</h1>
-            <h2 class="email">{{ }} your email</h2>
-            <label for="fn1">Your new firstname</label>
-            <input id="newfirstname" class="fn" :value="user.firstname">
-            <br>
-            <label for="fn1">Your new lastname</label>
-            <input id="newlastname" class="fn" :value="user.lastname">
-            <br>
-            <button @click="ChangeInfo">Change your info</button>
-        </div>
-      </span>
-    </div>
-  </template>
-  
-  
-  
-  
-  
-  
-  <script setup>
-    import { ref } from 'vue';
-  
-    const listResult = ref('');
-    const changeResult = ref('');
+  <div class="container">
+    <!-- Display user info -->
+    <span class="userInfoBox">
+      <div v-for="(user, index) in listResult" :key="index">
+        <h1>Your Info</h1>
+        <h2 class="email">{{ user.email }} is your email</h2>
+        <label for="fn1">Your firstname</label>
+        <input class="fn" readonly :value="user.firstname">
+        <br>
+        <label for="fn1">Your lastname</label>
+        <input class="fn" :value="user.lastname" readonly>
+        <br>
+      </div>
+      <button @click="GetInfo">Get Your Info</button>
+    </span>
 
-    const ChangeInfo = async () => {
-        const fname = document.getElementById('newfirstname');
-        const lname = document.getElementById('newlastname');
-        const tokenJson = localStorage.getItem('token');
-    
-        if (tokenJson) {
-        const tokenObj = JSON.parse(tokenJson);
-        const token = tokenObj.token;
-    
-        const myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${token}`);
-        myHeaders.append("Content-Type", "application/json");
+    <!-- Change user info -->
+    <span class="userInfoBox" v-if="listResult.length > 0">
+      <h1>Change Here</h1>
+      <h2 class="email">{{ listResult[0].email }} your email</h2>
+      <label for="fn1">Your new firstname</label>
+      <input id="newfirstname" class="fn" v-model="newFirstname">
+      <br>
+      <label for="fn1">Your new lastname</label>
+      <input id="newlastname" class="fn" v-model="newLastname">
+      <br>
+      <button @click="ChangeInfo">Change your info</button>
+    </span>
+  </div>
+</template>
+  
+  
+  
+  
+<script setup>
+import { ref } from 'vue';
 
-        const raw = JSON.stringify({
-            "firstname": fname.value,
-            "lastname": lname.value
-        });
+const listResult = ref([]); // Initialize as an empty array
+const newFirstname = ref('');
+const newLastname = ref('');
 
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-        };
-    
-        fetch("http://localhost:8080/api/v1/info/changeuser", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
-        }
+const ChangeInfo = async () => {
+  const tokenJson = localStorage.getItem('token');
+
+  if (tokenJson) {
+    const tokenObj = JSON.parse(tokenJson);
+    const token = tokenObj.token;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "firstname": newFirstname.value,
+      "lastname": newLastname.value
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
     };
 
-    
+    fetch("http://localhost:8080/api/v1/info/changeuser", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
+};
 
-    const GetInfo = async () => {
-        const tokenJson = localStorage.getItem('token');
-    
-        if (tokenJson) {
-        const tokenObj = JSON.parse(tokenJson);
-        const token = tokenObj.token;
-    
-        const myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${token}`);
-    
-        const requestOptions = {
-            method: "GET",
-            headers: myHeaders,
-            redirect: "follow"
-        };
-    
-        try {
-            const response = await fetch("http://localhost:8080/api/v1/info/user", requestOptions);
-            if (response.ok) {
-            const result = await response.json();
-            console.log(result);
-            listResult.value = result; // Save the result in the reactive variable
-            } else {
-            listResult.value = `Error: ${response.status} ${response.statusText}`;
-            }
-        } catch (error) {
-            console.error(error);
-            listResult.value = `Error: ${error.message}`;
-        }
-        } else {
-        listResult.value = 'No token found in localStorage.';
-        }
+const GetInfo = async () => {
+  const tokenJson = localStorage.getItem('token');
+
+  if (tokenJson) {
+    const tokenObj = JSON.parse(tokenJson);
+    const token = tokenObj.token;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
     };
-  </script>
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/info/user", requestOptions);
+      if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+        listResult.value = [result]; // Make it an array
+        newFirstname.value = result.firstname; // Set values for editing
+        newLastname.value = result.lastname;
+      } else {
+        listResult.value = `Error: ${response.status} ${response.statusText}`;
+      }
+    } catch (error) {
+      console.error(error);
+      listResult.value = `Error: ${error.message}`;
+    }
+  } else {
+    listResult.value = 'No token found in localStorage.';
+  }
+};
+</script>
+
   
   
   
